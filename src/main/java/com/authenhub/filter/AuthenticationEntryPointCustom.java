b78602f54b5645269b9b76aa3af801d53894c117
kacher2097 <ltuan997bg@gmail.com>
2025-04-18 17:29:05 +0700

    update

--- src/main/java/com/authenhub/filter/AuthenticationEntryPointCustom.java
+++ src/main/java/com/authenhub/filter/AuthenticationEntryPointCustom.java
@@ -1,31 +1,33 @@
-//package com.authenhub.filter;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.AllArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.AuthenticationEntryPoint;
-//import org.springframework.stereotype.Component;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//@Slf4j
-//@Component
-//@AllArgsConstructor
-//public class AuthenticationEntryPointCustom implements AuthenticationEntryPoint {
-//
-//    private final ObjectMapper objectMapper;
-//
-//    @Override
-//    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
-//        throws IOException {
-//        response.setStatus(HttpServletResponse.SC_OK);
-//        response.setHeader("Content-Type", "application/json");
-//        PrintWriter writer = response.getWriter();
-//        writer.println(objectMapper.writeValueAsString(BaseResponse.of(BaseResponseCode.FORBIDDEN)));
-//        writer.close();
-//    }
-//}
+package com.authenhub.filter;
+
+import com.authenhub.bean.common.ApiResponse;
+import com.authenhub.constant.enums.ApiResponseCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class AuthenticationEntryPointCustom implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setHeader("Content-Type", "application/json");
+        PrintWriter writer = response.getWriter();
+        writer.println(objectMapper.writeValueAsString(ApiResponse.error(ApiResponseCode.FORBIDDEN)));
+        writer.close();
+    }
+}
